77a57a4f3d957486eb2b5a5b4b3ce04413eed9af
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2023-12-06 20:32:54 -0500

    Fix tests, expanded content

--- public/asl-now-web/src/lib/programmes/default.ts
+++ public/asl-now-web/src/lib/programmes/default.ts
@@ -3,8 +3,6 @@ import {Dic} from "$lib/js/Dic";
 import {LetterHandler} from "$lib/js/handlers/letter";
 
 export const programmes = [
-    new Programme(new LetterHandler(0), [Dic.O
-        // , Dic.K, Dic.Y
-    ]),
-    new Programme(new LetterHandler(1), [Dic.K, Dic.O])
+    new Programme(new LetterHandler(0), [Dic.O, Dic.K, Dic.Y, Dic.O, Dic.C, Dic.Y, Dic.D, Dic.E]),
+    new Programme(new LetterHandler(0), [Dic.A, Dic.B, Dic.C, Dic.D, Dic.E, Dic.F, Dic.G, Dic.H, Dic.I, Dic.J, Dic.K, Dic.L, Dic.M, Dic.N, Dic.O, Dic.P, Dic.Q, Dic.R, Dic.S, Dic.T, Dic.U, Dic.V, Dic.W, Dic.X, Dic.Y, Dic.Z]),
 ]
