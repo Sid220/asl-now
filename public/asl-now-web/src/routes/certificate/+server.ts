2eb6bafb2fff93e05a57658c9c76313889fe5435
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2023-12-18 16:50:15 -0500

    Update certificate

--- public/asl-now-web/src/routes/certificate/+server.ts
+++ public/asl-now-web/src/routes/certificate/+server.ts
@@ -1,59 +1,96 @@
-import {error, redirect} from '@sveltejs/kit';
-import muhammara from 'muhammara';
+import {error} from '@sveltejs/kit';
+import muhammara, {createWriter, PDFStreamForResponse} from 'muhammara';
 import {version} from '$app/environment';
+import fs from "fs";
+import https from "https";
 
 export const config = {
-	runtime: 'edge'
+    runtime: 'edge'
 };
 
+const PDF_FILE = "https://cdn.jsdelivr.net/gh/Sid220/cdn@main/asl-now/Completion.pdf";
 
-export function GET({url}: { url: URL }) {
+function createCert(in_pdf: string, name: string): Promise<Response> {
+    const Recipe = muhammara.Recipe;
+    const pdfDoc = new Recipe(in_pdf, "out.pdf");
+    const now = new Date();
+    return new Promise((resolve) => {
+        pdfDoc
+            // edit 1st page
+            .editPage(1)
+            .text(name, 170, 250, {
+                color: "#000000",
+                size: 20
+            })
+            .text(version, 350, 328, {
+                color: "#000000",
+                size: 14
+            })
+            .text(now.getMonth() + "/" + now.getDay() + "/" + now.getFullYear(), 430, 328, {
+                color: "#000000",
+                size: 14
+            })
+            .text(Date.now().toString(), 692, 228, {
+                color: "#000000",
+                size: 20,
+                opacity: 0.5,
+                rotation: 90
+            })
+            .text("VeriNow!", 660, 210, {
+                color: "#000000",
+                size: 14
+            })
+            .text("VeriNow!", 660, 380, {
+                color: "#000000",
+                size: 14
+            })
+            .rectangle(660, 250, 50, 10, {
+                color: "#000000",
+                fill: "#000000",
+            })
+            .rectangle(660, 350, 50, 10, {
+                color: "#000000",
+                fill: "#000000",
+            })
+            .endPage()
+            .endPDF(() => {
+                const pdfContent = fs.readFileSync("out.pdf");
+                resolve(new Response(pdfContent, {
+                    headers: {
+                        'Content-Type': 'application/pdf',
+                        'Content-Disposition': 'attachment; filename="certificate.pdf"'
+                    }
+                }));
+            });
+    });
+}
+
+export async function GET({url}: { url: URL }) {
     const name = url.searchParams.get('name');
     if (!name) {
         throw error(400, 'Invalid name');
     }
-    const Recipe = muhammara.Recipe;
-    const pdfDoc = new Recipe("src/lib/pdfs/Completion.pdf", "static/output.pdf");
-    const now = new Date();
-    pdfDoc
-        // edit 1st page
-        .editPage(1)
-        .text(name, 170, 250, {
-            color: "#000000",
-            size: 20
-        })
-        .text(version, 350, 328, {
-            color: "#000000",
-            size: 14
-        })
-        .text(now.getMonth() + "/" + now.getDay() + "/" + now.getFullYear(), 430, 328, {
-            color: "#000000",
-            size: 14
-        })
-        .text(Date.now().toString(), 692, 228, {
-            color: "#000000",
-            size: 20,
-            opacity: 0.5,
-            rotation: 90
-        })
-        .text("VeriNow!", 660, 210, {
-            color: "#000000",
-            size: 14
-        })
-        .text("VeriNow!", 660, 380, {
-            color: "#000000",
-            size: 14
-        })
-        .rectangle(660, 250, 50, 10, {
-            color: "#000000",
-            fill: "#000000",
-        })
-        .rectangle(660, 350, 50, 10, {
-            color: "#000000",
-            fill: "#000000",
-        })
-        .endPage()
-        .endPDF();
 
-    throw redirect(302, '/output.pdf');
+    let filePromise = () => new Promise((resolve) => {
+        resolve(null);
+    });
+    if (!fs.existsSync("blank.pdf")) {
+        const file = fs.createWriteStream("blank.pdf");
+        filePromise = () => {
+            return new Promise((resolve) => {
+                https.get(PDF_FILE, function (response) {
+                    response.pipe(file);
+
+                    // after download completed close filestream
+                    file.on("finish", () => {
+                        file.close();
+                        console.log("Download Completed");
+                        resolve(null);
+                    });
+                });
+            });
+        }
+    }
+    await filePromise();
+    return await createCert("blank.pdf", name);
 }
