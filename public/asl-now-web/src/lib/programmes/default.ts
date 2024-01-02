77f6604734cb399a5ea97e9414983ca6bd307e0b
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2024-01-02 17:50:58 -0500

    New programme

--- public/asl-now-web/src/lib/programmes/default.ts
+++ public/asl-now-web/src/lib/programmes/default.ts
@@ -4,5 +4,17 @@ import {LetterHandler} from "$lib/js/handlers/letter";
 import {LetterHandlerVTwo} from "$lib/js/handlers/letter_vtwo";
 
 export const programmes = [
-    new Programme(new LetterHandlerVTwo(0), [Dic.O, Dic.K, Dic.Y, Dic.O, Dic.C, Dic.Y, Dic.D, Dic.E, Dic.A, Dic.B, Dic.C, Dic.D, Dic.E, Dic.F, Dic.G, Dic.H, Dic.I, Dic.J, Dic.K, Dic.L, Dic.M, Dic.N, Dic.O, Dic.P, Dic.Q, Dic.R, Dic.S, Dic.T, Dic.U, Dic.V, Dic.W, Dic.X, Dic.Y, Dic.Z]),
+    new Programme(new LetterHandlerVTwo(0), [
+        // Intro
+        Dic.O, Dic.K, Dic.Y, Dic.O, Dic.C, Dic.Y, Dic.D, Dic.E,
+        // Alphabet with recalling previous signs
+        Dic.A, Dic.B, Dic.C, Dic.A, Dic.D, Dic.E, Dic.F, Dic.G, Dic.E, Dic.H, Dic.I, Dic.J, Dic.F, Dic.K, Dic.L, Dic.G, Dic.M,
+        Dic.N, Dic.H, Dic.O, Dic.L, Dic.P, Dic.M, Dic.Q, Dic.N,
+        Dic.R, Dic.O, Dic.S, Dic.P, Dic.T, Dic.Q, Dic.R, Dic.U, Dic.S, Dic.V, Dic.T, Dic.V, Dic.W, Dic.X, Dic.Y, Dic.Z,
+        Dic.Y,
+        // Alphabet Backwards
+        Dic.Z, Dic.X, Dic.W, Dic.V, Dic.U, Dic.T, Dic.S, Dic.R, Dic.Q, Dic.P, Dic.O, Dic.N, Dic.M, Dic.L, Dic.K, Dic.J, Dic.I, Dic.H, Dic.G, Dic.F, Dic.E, Dic.D, Dic.C, Dic.B, Dic.A,
+        // Alphabet
+        Dic.B, Dic.C, Dic.D, Dic.E, Dic.F, Dic.G, Dic.H, Dic.I, Dic.J, Dic.K, Dic.L, Dic.M, Dic.N, Dic.O, Dic.P, Dic.Q, Dic.R, Dic.S, Dic.T, Dic.U, Dic.V, Dic.W, Dic.X, Dic.Y, Dic.Z
+    ]),
 ]
